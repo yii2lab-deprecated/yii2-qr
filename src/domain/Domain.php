13183ca9ffa86d445e6e80989db2cafc97fb058c
Vitaliy <theyamshikov@yandex.ru>
2018-10-21 22:56:39 +0600

    clean

--- src/domain/Domain.php
+++ src/domain/Domain.php
@@ -4,6 +4,12 @@ namespace yii2lab\qr\domain;
 
 use yii2lab\domain\enums\Driver;
 
+/**
+ * Class Domain
+ * 
+ * @property-read \yii2lab\qr\domain\interfaces\services\GeneratorInterface $generator
+ * @property-read \yii2lab\qr\domain\interfaces\repositories\RepositoriesInterface $repositories
+ */
 class Domain extends \yii2lab\domain\Domain {
 	
 	public function config() {
